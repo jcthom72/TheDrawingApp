6bc5ca586a4b51edd6ab7419f1a75e39a3141fab
student456773 <sbickel201@gmail.com>
2017-03-13 16:09:42 -0500

    Initial commit

---
+++ app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment3/thedrawingapp/MainActivity.java
@@ -0,0 +1,13 @@
+package csci4020.shawnbickel_judsonthomas.assignment3.thedrawingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
