c72ffd5d4d369edf11d30a2c1897af15e06dda25
student456773 <sbickel201@gmail.com>
2017-03-15 21:00:18 -0500

    added a ColorPicker Library from a GitHub repository; it uses the MIT license

--- app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment3/thedrawingapp/MainActivity.java
+++ app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment3/thedrawingapp/MainActivity.java
@@ -1,7 +1,14 @@
 package csci4020.shawnbickel_judsonthomas.assignment3.thedrawingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import petrov.kristiyan.colorpicker.ColorPicker;
+
+/* this is a ColorPicker library; here is the URL:
+    https://github.com/kristiyanP/colorpicker?utm_source=android-arsenal.com&utm_medium=referral&utm_campaign=3121 */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButton chooseColor = (ImageButton) findViewById(R.id.colorPicker);
+        chooseColor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ColorPicker colorPicker = new ColorPicker(MainActivity.this);
+                colorPicker.show();
+            }
+        });
+
 
     }
 }
